6a046e88c4bb5b07f1e22857a8aee9835d227767
johnpathadan <johnpathadan@gmail.com>
2023-02-06 23:20:12 +0530

    feat: useState added

---
+++ hooks-app/src/App.js
@@ -0,0 +1,12 @@
+import StateTutotal from "./Components/useStateTutorial";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <StateTutotal />
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks-app/src/Components/useStateTutorial.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StateTutotal = () => {
+  return <div>Hello World</div>;
+};
+
+export default StateTutotal;
