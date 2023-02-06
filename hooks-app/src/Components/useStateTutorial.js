bb5d4f315af87e63f44f606e56db47b1cbfa5c9a
johnpathadan <johnpathadan@gmail.com>
2023-02-06 23:38:06 +0530

    fix: useState ended

--- hooks-app/src/Components/useStateTutorial.js
+++ hooks-app/src/Components/useStateTutorial.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateTutotal = () => {
-  return <div>Hello World</div>;
+  const [inputValue, setInputValue] = useState("Pedro");
+
+  const onChange = (event) => {
+    let userValue = event.target.value;
+    setInputValue(userValue);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Enter your name" onChange={onChange} />
+      {inputValue}
+    </div>
+  );
 };
 
 export default StateTutotal;
