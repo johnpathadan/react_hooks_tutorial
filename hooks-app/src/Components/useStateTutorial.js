4fa08f56e7e651dcddf32eb8f2577ad6951f6fb8
johnpathadan <johnpathadan@gmail.com>
2023-02-09 22:18:30 +0530

    fix: useState ends

--- hooks-app/src/Components/useStateTutorial.js
+++ hooks-app/src/Components/useStateTutorial.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { Tab } from "@headlessui/react";
-import HelloWorld from "./helloWorld";
 
 const StateTutotal = () => {
   const [inputValue, setInputValue] = useState("Pedro");
@@ -15,22 +13,6 @@ const StateTutotal = () => {
       <div>
         <input type="text" placeholder="Enter your name" onChange={onChange} />
         {inputValue}
-      </div>
-      <div>
-        <Tab.Group>
-          <Tab.List>
-            <Tab>Tab 1</Tab>
-            <Tab>Tab 2</Tab>
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel>
-              <HelloWorld />
-            </Tab.Panel>
-            <Tab.Panel>
-              <HelloWorld /> ffds
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
       </div>
     </div>
   );
